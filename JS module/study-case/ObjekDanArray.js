9d6a8301380f1befb5525de8eaa52fbfada90127
Akbar Husein <muhakbarhusein5@gmail.com>
2022-11-12 22:14:45 +0700

    Study Case Object & Array

---
+++ JS module/study-case/ObjekDanArray.js
@@ -0,0 +1,23 @@
+/** 1 :
+ * Buatlah sebuah Array mahasiswa yang berisi dua buah object
+ * yang memiliki property nama <string>, stambuk <number>, dan instansi <string>.
+ */
+// TODO 1: Ketikkan kode Anda di bawah ini!
+
+/** 2 : Tampilkan array mahasiswa */
+// TODO 2: Ketikkan kode Anda di bawah ini!
+
+/** 3 :
+ * Setelah membuat Array mahasiswa, tambahkan satu buah object dengan property yang sama seperti object sebelumnya
+ * dengan menggunakan Array methods.
+ */
+// TODO 3: Ketikkan kode Anda di bawah ini!
+
+/** 4 : Hapus index 1 pada Array mahasiswa dengan menggunakan Array methods */
+// TODO 4: Ketikkan kode Anda di bawah ini!
+
+/** 5 : Tampilkan array mahasiswa dengan menggunkan for loop atau yang sejenisnya */
+// TODO 5: Ketikkan kode Anda di bawah ini!
+
+/** 6 : Tampilkan nama pada Object didalam Array mahasiswa dengan menggunakan for loop atau yang sejenisnya*/
+// TODO 6: Ketikkan kode Anda di bawah ini!
