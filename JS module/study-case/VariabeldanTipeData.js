86fbbc470871db9899a79d484a1517110691ec88
AkbarHusein <muhakbarhusein5@gmail.com>
2022-11-12 16:01:20 +0700

    studycase tipr data

---
+++ JS module/study-case/VariabeldanTipeData.js
@@ -0,0 +1,12 @@
+/*
+
+Buatlah variabel NamaDepan, NamaBelakang, Angkatan, Instansi dengan ketentuan:
+*  - NamaDepan: bertipe data string, dengan nilai nama depan Anda.
+*  - NamaBelakang: bertipe data string, dengan nilai nama belakang Anda.
+*  - Angkatan: bertipe data number, dengan nilai Angkatan Anda.
+*  - Instansi: bertipe data String, sesuai dengan Instansi Anda.
+*  - Mahasiswa Aktif: Bertipe data boolean Anda:
+
+*/
+
+// TODO:
