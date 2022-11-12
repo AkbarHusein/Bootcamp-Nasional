ee40231f1fadb6b90fb25e4a87b68c95a01b5103
Akbar Husein <muhakbarhusein5@gmail.com>
2022-11-13 00:01:48 +0700

    if-else study case

---
+++ JS module/study-case/function.js
@@ -0,0 +1,34 @@
+/** Jangan mengubah Kode di Bawah ini! */
+const mahasiswa = [
+  {
+    nama: 'Royhan',
+    nim: 20017001,
+    mk: [
+      { nama: 'Pemrograman Web', kuis: 65, uts: 80, uas: 75 },
+      { nama: 'Mobile Programming', kuis: 70, uts: 80, uas: 70 },
+      { nama: 'Ethical Hacking', kuis: 75, uts: 70, uas: 75 },
+    ],
+  },
+  {
+    nama: 'Reyhan',
+    stambuk: 20017002,
+    mk: [
+      { nama: 'Pemrograman Web', kuis: 70, uts: 80, uas: 75 },
+      { nama: 'Mobile Programming', kuis: 70, uts: 80, uas: 80 },
+      { nama: 'Ethical Hacking', kuis: 80, uts: 70, uas: 75 },
+    ],
+  },
+];
+
+/** 1 :
+ * Buatlah sebuah function tanpa parameter yang berfungsi untuk
+ * menampilkan nilai uas mk Pemrograman Web pada Array of Object mahasiswa
+ */
+// TODO 1: Ketikkan kode Anda di bawah ini!
+
+/** 2 :
+ * Buatlah sebuah function dengan parameter nama yang berfungsi untuk
+ * menampilkan nilai uas mk Pemrograman Web pada Array of Object mahasiswa
+ * berdasarkan nama mahasiswa
+ */
+// TODO 2: Ketikkan kode Anda di bawah ini!
