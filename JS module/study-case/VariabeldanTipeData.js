b704291973fe53cb4d552c2d0bab5af51205988a
Husein <70703161+AkbarHusein@users.noreply.github.com>
2022-11-12 16:03:28 +0700

    Update VariabeldanTipeData.js

--- JS module/study-case/VariabeldanTipeData.js
+++ JS module/study-case/VariabeldanTipeData.js
@@ -1,11 +1,11 @@
 /*
 
-Buatlah variabel NamaDepan, NamaBelakang, Angkatan, Instansi dengan ketentuan:
+Buatlah variabel NamaDepan, NamaBelakang, Angkatan, Instansi dengan ketentuan. kemudian tampilkan setiap nilai variabel:
 *  - NamaDepan: bertipe data string, dengan nilai nama depan Anda.
 *  - NamaBelakang: bertipe data string, dengan nilai nama belakang Anda.
 *  - Angkatan: bertipe data number, dengan nilai Angkatan Anda.
 *  - Instansi: bertipe data String, sesuai dengan Instansi Anda.
-*  - Mahasiswa Aktif: Bertipe data boolean Anda:
+*  - Mahasiswa Aktif: Bertipe data boolean:
 
 */
 
